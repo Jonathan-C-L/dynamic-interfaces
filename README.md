b86f7a958a5261688dc4f5e6d418a3ac19823a4c
Jonathan-C-L <jle16@nait.ca>
2025-08-26 21:24:08 -0600

    setup the webpage to hold the image carousel

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Project Summary
-This is a small project to create a dropdown menu that can be activated on click or by hovering.
+This is a small project to create a dropdown menu that can be activated on click or by hovering. There is also an image carousel that will have an option to switch manually or automatically.
 
 ## Things learned throughout this project:
 - Have to make sure webpack config files have the correct paths to the index files or else webpack will not work...
--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-const main = document.querySelector("main");
+const leftSidebar = document.querySelector(".left-sidebar");
 comboDropdown();
 
 // functions
 // combination of both the hover and click mechanisms - feels more fluid and natural to have both options
 function comboDropdown(){
   const dropdownElements = renderDropdownMenu();
-  main.appendChild(createNewContainer("click", [newImage(dropdownElements.dropIcon, "dropdown icon")]));
+  leftSidebar.appendChild(createNewContainer("click", [newImage(dropdownElements.dropIcon, "dropdown icon")]));
 
   const clickDropdown = document.querySelector(".click");
   clickDropdown.classList.add("dropdown");
@@ -26,34 +26,6 @@ function comboDropdown(){
 
   dropOnClick(".click", clickDropdown);
   dropOnHover(".click", clickDropdown);
-}
-// just the click mechanism for the dropdown menu
-function clickDropdown(){
-  const dropdownElements = renderDropdownMenu();
-  main.appendChild(createNewContainer("click", [newImage(dropdownElements.dropIcon, "dropdown icon")]));
-
-  const clickDropdown = document.querySelector(".click");
-  clickDropdown.classList.add("dropdown");
-
-  for(let item of dropdownElements.menu){
-    clickDropdown.appendChild(item);
-  }
-
-  dropOnClick(".click", clickDropdown);
-}
-// just the hover mechanism for the dropdown menu
-function hoverDropdown(){
-  const dropdownElements = renderDropdownMenu();
-  main.appendChild(createNewContainer("hover", [newImage(dropdownElements.dropIcon, "dropdown icon")]));
-  
-  const hoverDropdown = document.querySelector(".hover");
-  hoverDropdown.classList.add("dropdown");
-
-  for(let item of dropdownElements.menu){
-    hoverDropdown.appendChild(item);
-  }
-
-  dropOnHover(".hover", hoverDropdown);
 }
 
 // event listeners
